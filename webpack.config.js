9440e28052aa584ebc932d378d19aaa70fa38f1f
Abraham Huacchillo <huacchillocastillo@gmail.com>
2022-04-05 11:21:02 -0500

    loader at images

--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,10 @@ module.exports = {
                 use :[MiniCssExtractPlugin.loader,'css-loader',
                 'stylus-loader'
                 ],
+            },
+            {
+                test:/\.png/,
+                type:'asset/resource'
             }
         ]
     },
